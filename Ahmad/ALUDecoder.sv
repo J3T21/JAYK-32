03b3c7f172f44dacb530eedc632240f076723dfe
ahumayde <ahumayde@gmail.com>
2022-11-24 14:00:24 +0000

    modifying ALUDecoder to include more instructions

--- Ahmad/ALUDecoder.sv
+++ Ahmad/ALUDecoder.sv
@@ -11,8 +11,14 @@ module ALUDecoder(
             2'b00: 
                 ALUControl = 3'b000;
             // Branch 
-            2'b01: 
-                ALUControl = 3'b001;
+            2'b01: case (func3)
+                // Branch Equal - beq
+                3'b00:  ALUControl = 3'b001;
+                // Branch Not Equal - bne
+                3'b01:  ALUControl = 3'b001;
+                // Other Branches maybe use slt aluctrl
+                default: ALUControl = 3'b001;  
+            endcase
             // ALU
             2'b10: case(func3)
                 // Add Sub - add, sub
@@ -24,9 +30,20 @@ module ALUDecoder(
                     default:
                         ALUControl = 3'b000;
                 endcase
+                // Shift Left
+                3'b001:    ALUControl = 3'b110;
                 // Set Less Than - slt 
-                3'b010:
-                    ALUControl = 3'b100;
+                3'b010:    ALUControl = 3'b101;
+                // Set Less Than Unsigned - sltu
+                3'b011:    ALUControl = 3'b101; // CHANGE
+                // XOR
+                3'b100:    ALUControl = 3'b100;
+                // Right Shift
+                3'b101: case(func7[5])
+                    1'b0:  ALUControl = 3'b111;
+                    // NEEDS CHANGING FOR ARITHMETIC SHIFT
+                    1'b1:  ALUControl = 3'b111; // CHANGE
+                endcase
                 // Or - or
                 3'b110:
                     ALUControl = 3'b011;
