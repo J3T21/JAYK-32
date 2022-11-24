5377a4ba11af41afbb83671a02bd98dfacf74f70
ahumayde <ahumayde@gmail.com>
2022-11-24 14:01:42 +0000

    modified alu to include all instructions

--- Ahmad/ALU.sv
+++ Ahmad/ALU.sv
@@ -11,11 +11,23 @@ module ALU #(
     always_comb begin
         EQ = (ALUop1 == ALUop2);
         case (ALUctrl)
+            // Add
             3'b000:    ALUout = ALUop1 + ALUop2;
+            // Sub
             3'b001:    ALUout = ALUop1 - ALUop2;
+            // And
             3'b010:    ALUout = ALUop1 & ALUop2;
+            // OR
             3'b011:    ALUout = ALUop1 | ALUop2;
-            3'b100:    ALUout = {{31{1'b0}},{ALUop1 < ALUop2}};
+            // XOR
+            3'b100:    ALUout = ALUop1 ^ ALUop2;
+            // SLT
+            3'b101:    ALUout = {{31{1'b0}},{ALUop1 < ALUop2}};
+            // Shift Left
+            3'b110:    ALUout = ALUop1 << ALUop2;
+            // Shift Right
+            3'b111:    ALUout = ALUop1 >> ALUop2;
+            // Default
             default:   ALUout = 32'b0;
         endcase
         $display("    New line    ");
