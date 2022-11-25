fb2ce2cc2a0134ab3cf2ccea24f846c0f2711876
ahumayde <ahumayde@gmail.com>
2022-11-25 03:48:33 +0000

    renamed variables

--- Ahmad/SignExtend.sv
+++ Ahmad/SignExtend.sv
@@ -1,23 +1,23 @@
 module SignExtend(
-    input  logic [31:0] instr,
+    input  logic [31:7] Instr,
     input  logic [2:0]  ImmSrc,
-    output logic [31:0] ImmOp
+    output logic [31:0] ImmExt
 );
 
     always_comb 
         case (ImmSrc)
         // Immediate
-            3'b000:  ImmOp = {{20{instr[31]}},{instr[31:20]}};
+            3'b000:  ImmExt = {{20{Instr[31]}},{Instr[31:20]}};
         // Upper Immediate
-            3'b001:  ImmOp = {{instr[31:12]},{12{1'b0}}}; 
+            3'b001:  ImmExt = {{Instr[31:12]},{12{1'b0}}}; 
         // Store
-            3'b010:  ImmOp = {{20{instr[31]}},{instr[31:25]},{instr[11:7]}};
+            3'b010:  ImmExt = {{20{Instr[31]}},{Instr[31:25]},{Instr[11:7]}};
         // Branch
-            3'b011:  ImmOp = {{21{instr[31]}},{instr[7]},{instr[30:25]},{instr[11:8]}};
+            3'b011:  ImmExt = {{21{Instr[31]}},{Instr[7]},{Instr[30:25]},{Instr[11:8]}};
         // Jump      
-            3'b100:  ImmOp = {{13{instr[31]}},{instr[19:12]},{instr[20]},{instr[30:21]}};
+            3'b100:  ImmExt = {{13{Instr[31]}},{Instr[19:12]},{Instr[20]},{Instr[30:21]}};
         // Default
-            default: ImmOp = {32{1'b0}};
+            default: ImmExt = {32{1'b0}};
         endcase
     
 endmodule
