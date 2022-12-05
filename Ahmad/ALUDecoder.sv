078133bd0c9634dc0cc2a1214baa781dbc450ef4
ahumayde <ahumayde@gmail.com>
2022-12-05 23:41:48 +0000

    fixed funct7[5] issue

--- Ahmad/ALUDecoder.sv
+++ Ahmad/ALUDecoder.sv
@@ -18,7 +18,7 @@ module ALUDecoder (
       // Branch (bne/beq = 0000, blt/bge = 0010, bltu/bgeu = 0011)
       2'b01:   ALUControl = funct3[2] ? {{3'b001}, {funct3[1]}} : 4'b0000;
       // ALU bit 3 only matters for add/sub vs rsl/rsa so we can us funct3[2] to determine it
-      2'b10:   ALUControl = {{funct3[2] ? {funct7} : {funct7[5] & op5}}, {funct3}};
+      2'b10:   ALUControl = {{funct3[2] ? {funct7} : {funct7 & op5}}, {funct3}};
       // Default
       default: ALUControl = 4'b0000;
     endcase
